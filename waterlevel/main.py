52c87c642aa44bdb6bf46e51febddc6dbb5f6eee
Akmal <hanzomainer1488@gmail.com>
2024-06-20 21:05:22 +1000

    Добавить основной функционал
    Добавлен рабочий графический интерфейс с тестовыми данными. Настроено
    отображение графика и вывод списка уровня реки в определенные даты

---
+++ waterlevel/main.py
@@ -0,0 +1,65 @@
+import datetime
+import sys
+from random import randint
+from PyQt6.QtWidgets import QTableWidgetItem
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from PyQt6 import QtWidgets
+
+from mainwindow import Ui_MainWindow
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Создаем холст для графика
+        self.canvas = MplCanvas(self, width=12, height=4, dpi=100)
+
+        # Добавляем холст в layout plotWidget
+        layout = QtWidgets.QVBoxLayout(self.ui.plotWidget)
+        layout.addWidget(self.canvas)
+
+        # Подключаем событие нажатия кнопки к методу on_generate_clicked
+        self.ui.generate.clicked.connect(self.on_generate_clicked)
+
+        # Отображаем график
+        self.plot()
+
+    def plot(self, x=["01.01.24"], y=[0]):
+        self.canvas.axes.clear()
+        self.canvas.axes.plot(x, y)
+        self.canvas.draw()
+
+    def on_generate_clicked(self):
+        a = self.ui.dateFrom.date().toPyDate()
+        b = self.ui.dateTo.date().toPyDate()
+        delta = (b - a).days + 1
+        table = self.ui.waterLevel
+        x = [(a + datetime.timedelta(days=x)).strftime("%d.%m.%y") for x in range(delta)]
+        y = [randint(1, 100) for i in range(delta)]
+        table.setColumnCount(2)
+        table.setRowCount(delta)
+        table.setColumnWidth(0, 480)
+        table.setColumnWidth(1, 480)
+        table.setHorizontalHeaderLabels(["Дата", "Уровень воды"])
+        for i, elem in enumerate(x):
+            table.setItem(i, 0, QTableWidgetItem(elem))
+            table.setItem(i, 1, QTableWidgetItem(str(y[i])))
+        self.plot(x, y)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = MainWindow()
+    MainWindow.show()
+    sys.exit(app.exec())
---
+++ waterlevel/mainwindow.py
@@ -0,0 +1,91 @@
+import datetime
+
+from PyQt6 import QtCore, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1280, 720)
+        MainWindow.setStyleSheet("background: qlineargradient(\n"
+"                spread:pad, x1:0, y1:0, x2:1, y2:1,\n"
+"                stop:0 rgba(85, 0, 255, 255),\n"
+"                stop:1 rgba(170, 85, 255, 255)\n"
+"            );")
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setStyleSheet("QWidget {\n"
+"            color: white;\n"
+"            font-size: 12pt;\n"
+"        }")
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.plotWidget = QtWidgets.QWidget(parent=self.centralwidget)
+        self.plotWidget.setMinimumSize(QtCore.QSize(1200, 400))
+        self.plotWidget.setObjectName("plotWidget")
+        self.gridLayout.addWidget(self.plotWidget, 0, 0, 1, 4)
+        self.waterLevel = QtWidgets.QTableWidget(parent=self.centralwidget)
+        self.waterLevel.setStyleSheet("background-color: white;\n"
+"            color: black;\n"
+"            border-radius: 10px;\n"
+"            padding: 5px;")
+        self.waterLevel.setObjectName("waterLevel")
+        self.waterLevel.setColumnCount(0)
+        self.waterLevel.setRowCount(0)
+        self.gridLayout.addWidget(self.waterLevel, 2, 1, 1, 1)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.dateFrom = QtWidgets.QDateEdit(parent=self.centralwidget)
+        self.dateFrom.setStyleSheet("background-color: white;\n"
+"            color: black;\n"
+"            border-radius: 10px;\n"
+"            padding: 5px;")
+        self.dateFrom.setObjectName("dateFrom")
+        self.verticalLayout.addWidget(self.dateFrom)
+        self.dateFrom.setDate(datetime.date(2024, 1, 1))
+        self.label = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label.setStyleSheet("background-color: white;\n"
+"            color: black;\n"
+"            border-radius: 10px;\n"
+"            padding: 5px;")
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.gridLayout.addLayout(self.verticalLayout, 1, 0, 1, 1)
+        self.generate = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.generate.setMinimumSize(QtCore.QSize(0, 0))
+        self.generate.setStyleSheet("background-color: white;\n"
+"            color: black;\n"
+"            border-radius: 10px;\n"
+"            padding: 5px;")
+        self.generate.setStyleSheet("QPushButton:hover {background-color: #3700B3;}")
+        self.generate.setObjectName("generate")
+        self.gridLayout.addWidget(self.generate, 1, 1, 1, 1)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.dateTo = QtWidgets.QDateEdit(parent=self.centralwidget)
+        self.dateTo.setStyleSheet("background-color: white;\n"
+"            color: black;\n"
+"            border-radius: 10px;\n"
+"            padding: 5px;")
+        self.dateTo.setObjectName("dateTo")
+        self.dateTo.setDate(datetime.date(2024, 1, 1))
+        self.verticalLayout_2.addWidget(self.dateTo)
+        self.label_2 = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label_2.setStyleSheet("background-color: white;\n"
+"            color: black;\n"
+"            border-radius: 10px;\n"
+"            padding: 5px;")
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.gridLayout.addLayout(self.verticalLayout_2, 1, 2, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("WaterLevelAI", "WaterLevelAI"))
+        self.label.setText(_translate("MainWindow", "Начало"))
+        self.generate.setText(_translate("MainWindow", "Предсказать"))
+        self.label_2.setText(_translate("MainWindow", "Конец"))
